eb79875d25af96dd382ad4e1a6cea3cb13c7946a
JuanmaInv <invaldijuanmartin@gmail.com>
2025-04-26 13:51:59 -0300

    tasa de cambio

---
+++ main.py
@@ -0,0 +1,8 @@
+# Tasas de Cambio Fijas
+
+TASAS_CAMBIO = {
+    'USD': {'EUR': 0.88, 'ARS': 1200, 'REA':5.69},
+    'EUR': {'USD': 0.88, 'ARS': 1200, 'BRL':5.69},
+    'ARS': {'USD': 0.88, 'EUR': 1200, 'BRL':5.69},
+    'BRL': {'USD': 0.88, 'EUR': 1200, 'ARS':5.69},
+}
